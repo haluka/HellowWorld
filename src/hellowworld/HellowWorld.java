0fc4699c05357e2c2faaa244570500d2ce51b498
g12911hh <g12911hh@s30444.tsuda.ac.jp>
2014-04-11 11:05:47 +0900

    HelloとWorldです

---
+++ src/hellowworld/HellowWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellowworld;
+
+/**
+ *
+ * @author g12911hh
+ */
+public class HellowWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
